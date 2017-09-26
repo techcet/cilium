cb7f3f4389fcc8a73a9d092f230120781b489dfc
Cynthia Thomas <techcet@users.noreply.github.com>
2017-09-26 10:57:51 -0700

    Update cilium-mesos-demo.sh

--- examples/mesos/cilium-mesos-demo.sh
+++ examples/mesos/cilium-mesos-demo.sh
@@ -24,10 +24,13 @@ desc_rate "Next, start Marathon, the container scheduler".
 run "./start_marathon.sh"
 desc_rate "Start the web-server application."
 run "eval curl -i -H 'Content-Type: application/json' -d @web-server.json 127.0.0.1:8080/v2/apps"
+desc_rate "Next, start the goodclient task, retrieving URLs from the web-server."
 run "eval curl -i -H 'Content-Type: application/json' -d @goodclient.json 127.0.0.1:8080/v2/apps"
+desc_rate "Finally, start the badclient task, retrieving URLs from the web-server."
 run "eval curl -i -H 'Content-Type: application/json' -d @badclient.json 127.0.0.1:8080/v2/apps"
+desc_rate "Cilium represents these workloads as endpoints, as observed with the following output:"
 run "cilium endpoint list"
-
+desc_rate "Let's observe what these tasks are doing by looking at the logs."
 #run "screen -S goodclient"
 trap ' ' INT
 desc_rate "This is the goodclient's log."
@@ -38,10 +41,9 @@ desc_rate "This is the badclient's log."
 run "./tail_client.sh badclient"
 # hit CTRL-c
 desc_rate "With no policy enforced, both the goodclient and badclient can access /public and /private URLs from the web-server."
-
 desc_rate "Let's apply a Layer-7 policy that only allows the goodclient to access the /public URL."
 run "cilium policy import l7-policy.json"
-desc_rate "We can observe that the policy got enforced with the following output."
+desc_rate "We can observe that the policy got enabled with the following output."
 run "cilium endpoint list"
 sleep 2
 desc_rate "Let's check the goodclient's log again."
@@ -52,8 +54,9 @@ desc_rate "There you have it! Cilium enforces L7 policy to protect the /private 
 desc_rate "If you want to try out this demo yourself, you can do so by    
  following the steps at: http://www.cilium.io/try-mesos              
                                                                       
- If you want to learn more about Cilium:                                                                                        
-  - Visit: https://www.cilium.io/                                                                                             
-  - Join us on slack: https://cilium.herokuapp.com/"                                                                         
+ If you want to learn more about Cilium: 
+  - Visit: https://www.cilium.io/
+  - Join us on slack: https://cilium.herokuapp.com/
+ Thanks for watching!"
 
 trap "echo" EXIT
